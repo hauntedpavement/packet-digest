86fd55b2114ac6773c8f2fea02d9ef54ea6400b6
hauntedpavement <haunted.pavement@gmail.com>
2022-10-07 12:26:20 -0500

    p1

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,18 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Packet-Digest, a Blog',
+  author: 'Danby',
+  headerTitle: 'Packet-Digest',
+  description: 'A blog of internet history and homelabbing',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://packet-digest.vercel.app/',
+  siteRepo: 'https://github.com/hauntedpavement/packet-digest',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
+  github: 'https://github.com/hauntedpavement',
   twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
